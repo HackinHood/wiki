609e67b4735c05bf423d8d1e17309a6ad908e5b5
Ridhwan Hussain <ridhwanplayzzz@gmail.com>
2025-04-12 23:43:53 +0100

    Add API route for creating post

---
+++ app/api/posts/route.ts
@@ -0,0 +1,46 @@
+import { auth } from "@/lib/auth";
+import clientPromise from "@/lib/mongodb";
+
+export const GET = async (req: Request) => {
+    const client = await clientPromise;
+    const db = client.db("main-data");
+    const posts = await db.collection("posts").find().toArray();
+
+    return new Response(JSON.stringify(posts), {
+        status: 200,
+        headers: {
+            "Content-Type": "application/json",
+        },
+    });
+}
+
+export const POST = async (req: Request) => {
+    const session = auth.api.getSession(req)
+    if (!session) {
+        return new Response("Unauthorized", { status: 401 });
+    }
+
+    const client = await clientPromise;
+    const db = client.db("main-data");
+    const body = await req.json();
+
+    const { title, content } = body;
+
+    if (!title || !content) {
+        return new Response(JSON.stringify({ error: "Title and content are required" }), {
+            status: 400,
+            headers: {
+                "Content-Type": "application/json",
+            },
+        });
+    }
+
+    const post = await db.collection("posts").insertOne({ title, content });
+
+    return new Response(JSON.stringify(post), {
+        status: 201,
+        headers: {
+            "Content-Type": "application/json",
+        },
+    });
+}
