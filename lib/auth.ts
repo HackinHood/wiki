663c26f0bddb5bf3f5aeefd6615eab6402ed9282
Ridhwan Hussain <ridhwanplayzzz@gmail.com>
2025-04-12 23:06:51 +0100

    Implement auth pgaes

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,5 +1,10 @@
 import { betterAuth } from "better-auth";
+import { mongodbAdapter } from "better-auth/adapters/mongodb";
+import clientPromise from "./mongodb";
+
+const client = await clientPromise;
+const db = client.db("auth");
 
 export const auth = betterAuth({
-    
+    database: mongodbAdapter(db),
 })
